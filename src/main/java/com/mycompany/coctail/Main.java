ed058be2db9448115bb21e5a6bdbb7703a7b695a
Yousef Sultan <yousefsultan800@gmail.com>
2024-05-01 15:50:53 +0300

    adding the Logger Feature to the project.

--- src/main/java/com/mycompany/coctail/Main.java
+++ src/main/java/com/mycompany/coctail/Main.java
@@ -8,8 +8,15 @@ public class Main {
         
     public static void main(String args[]){
         
+        Logger myLogger = new MyLogger();
+        Logger fileLogger = new FileLogger("test.txt");
         
+        myLogger.log("Logging message using MyLogger.");
+        fileLogger.log("Logging message using FileLogger.");
         
+        if(fileLogger instanceof FileLogger){
+            ((FileLogger) fileLogger).close();
+        }
     }
     
 }
